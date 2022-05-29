f1678598f09d3b97426a4a1a32aa9bb148e47cc8
unknown <saintgray0@gmail.com>
2022-05-29 21:14:32 +0900

    react self study - final project

---
+++ FirstReact/firstreact/src/components/layout/footer.js
@@ -0,0 +1,41 @@
+import './../../style/footer.css';
+
+
+function Footer(){
+    return(
+
+        <div id="footerwrap">
+            <div className="d-flex flex-row flex-wrap justify-content-center" id="innerfooterwrap">
+                <img src='/images/logo.svg' className="mx-4" id="logo"/>
+                <div className="f-info mx-4">
+                    <ul className="f-list">
+                        <li><span className="head">회사소개</span></li>
+                        <li>알려드림</li>
+                        <li>채용안내</li>
+                    </ul>
+                </div>
+
+                <div className="f-info mx-4">
+                    <ul className="f-list">
+                    <li><span className="head">고객안내</span></li>
+                    <li>이용안내</li>
+                        <li>이용약관</li>
+                    </ul>
+                </div>
+
+                <div className="f-info mx-4">
+                    <ul className="f-list">
+                        <li><a className="head" href="#">고객센터</a></li>
+                        <li><a href="#">공지사항</a></li>
+                        <li><a href="#">1:1문의</a></li>
+                        <li>자주 묻는 질문</li>      
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+
+export default Footer;
--- FirstReact/firstreact/src/index.js
+++ FirstReact/firstreact/src/index.js
@@ -5,8 +5,8 @@ import ReactDOM from 'react-dom';
 // 가상 요소를 정의한 JSX 파일 (App.js)
 // from 뒤의 경로 jsx 파일을 가져와서 Header (또는 Footer) 라는 값 이름으로 쓰겠다는 말이므로
 // 각각의 jsx 가 export 하는 함수 이름과 반드시 일치하지 않아도 된다(메소드와 유사)
-import Header from './layout/headerComponent';
-import Footer from './layout/footerComponent';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 import LocationQuickSearch from './home/search/locationQuickSearch'
 
 
@@ -15,27 +15,27 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Header />
-  </React.StrictMode>,
-  document.getElementById('header')
+ReactDOM.render( 
+    <React.StrictMode >
+    <Header / >
+    </React.StrictMode>,
+    document.getElementById('header')
+);
+
+ReactDOM.render( 
+    <React.StrictMode >
+    <Footer / >
+    </React.StrictMode>,
+    document.getElementById('footer')
 );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Footer />
-  </React.StrictMode>,
-  document.getElementById('footer')
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <LocationQuickSearch />
-  </React.StrictMode>,
-  document.getElementById('location')
-)
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    <React.StrictMode >
+    <LocationQuickSearch / >
+    </React.StrictMode>,
+        document.getElementById('location')
+    )
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
